bfe82e44f7a3ecdd62cdd04fcf6f5d6b364e3d88
Justin Chang <changguangyu@yahoo.com>
2020-08-24 14:36:09 -0700

    api functions finish

--- client/src/api_wrapper.js
+++ client/src/api_wrapper.js
@@ -141,6 +141,56 @@ const getCategory = (id) => {
 };
 
 /**
+ * Creates a new category
+ * @param {*} boardId Category's board's id
+ * @param {*} category Name of the new category
+ */
+const newCategory = (boardId, category) => {
+    let http = new XMLHttpRequest();
+    let board = getBoard(boardId);
+    let doc = {
+        name: category,
+        board: boardId,
+        position: board.categories.length,
+        cards: [],
+    };
+    http.open("POST", CATEGORIES_URL, false);
+    http.setRequestHeader("Content-Type", "application/json");
+    http.send(JSON.stringify(doc));
+    return http.responseText;
+};
+
+/**
+ * Changes info of category; use `null` if data not changed
+ * @param {*} id Id of category to change
+ * @param {*} name New name of category
+ * @param {*} position New position of category
+ */
+const editCategory = (id, name, position) => {
+    let http = new XMLHttpRequest();
+    let category = getCategory(id);
+    let url = CATEGORIES_URL + id + "/";
+    if (name != null) category.name = name;
+    if (position != null) category.position = position;
+    http.open("PUT", url, false);
+    http.setRequestHeader("Content-Type", "application/json");
+    http.send(JSON.stringify(category));
+    return http.responseText;
+};
+
+/**
+ * Deletes a board
+ * @param {*} id Id of board to delete
+ */
+const deleteCategory = (id) => {
+    let http = new XMLHttpRequest();
+    let url = CATEGORIES_URL + id + "/";
+    http.open("DELETE", url, false);
+    http.send(null);
+    return http.responseText;
+};
+
+/**
  * Returns JSON with data of card
  * @param {*} id Id of the board
  */
@@ -152,7 +202,60 @@ const getCard = (id) => {
     return http.responseText;
 };
 
-console.log(deleteBoard(3));
+/**
+ * Creates a new board
+ * @param {*} categoryId Board's owner's username
+ * @param {*} card Name of the new board
+ */
+const newCard = (categoryId, card) => {
+    let http = new XMLHttpRequest();
+    let category = getCategory(categoryId);
+    let doc = {
+        title: card,
+        description: "",
+        color: "white",
+        category: categoryId,
+        position: category.cards.length,
+    };
+    http.open("POST", CARDS_URL, false);
+    http.setRequestHeader("Content-Type", "application/json");
+    http.send(JSON.stringify(doc));
+    return http.responseText;
+};
+
+/**
+ * Changes info of card; use `null` if data not changed
+ * @param {*} id Id of card to change
+ * @param {*} title New title of card
+ * @param {*} description New description of card
+ * @param {*} color New color of card
+ * @param {*} position New position of card
+ */
+const editCard = (id, title, description, color, position) => {
+    let http = new XMLHttpRequest();
+    let card = getCard(id);
+    let url = CARDS_URL + id + "/";
+    if (title != null) card.title = title;
+    if (description != null) card.description = description;
+    if (color != null) card.color = color;
+    if (position != null) board.position = position;
+    http.open("PUT", url, false);
+    http.setRequestHeader("Content-Type", "application/json");
+    http.send(JSON.stringify(board));
+    return http.responseText;
+};
+
+/**
+ * Deletes a card
+ * @param {*} id Id of card to delete
+ */
+const deleteCard = (id) => {
+    let http = new XMLHttpRequest();
+    let url = CARDS_URL + id + "/";
+    http.open("DELETE", url, false);
+    http.send(null);
+    return http.responseText;
+};
 
 module.exports = {
     getUser,
@@ -162,4 +265,12 @@ module.exports = {
     newBoard,
     editBoard,
     deleteBoard,
+    getCategory,
+    newCategory,
+    editCategory,
+    deleteCategory,
+    getCard,
+    newCard,
+    editCard,
+    deleteCard,
 };
