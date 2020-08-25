0f7632c0d2ea9db2a649df16b859daac5854e742
Justin Chang <changguangyu@yahoo.com>
2020-08-24 18:48:02 -0700

    luh mao

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function Login(props) {
+    return <div className="background"></div>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <Route exact path="/" component={Login} />
+        <Route path="/board" component={App} />
+    </Router>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
