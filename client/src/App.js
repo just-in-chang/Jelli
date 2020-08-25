4a9b35a355c61136d0c9cb4559dbac0cda4d6c52
Andrew Chen <andrewchen118@gmail.com>
2020-08-24 18:45:25 -0700

    f

--- client/src/App.js
+++ client/src/App.js
@@ -11,99 +11,105 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  let list = [];
-  for (let i = 0; i < 10; i++) list.push("hi");
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    let list = [];
+    for (let i = 0; i < 10; i++) list.push("hi");
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  return (
-    <div className="background">
-      <div>
-        <NavBar variant="light" className="navbar">
-          <NavBar.Brand href="#">
-            <img
-              src="jelli.png"
-              width="30"
-              height="26"
-              className="d-inline-block align-top"
-              alt="Jelli logo"
-            />
-            {"  "}Jelli
-          </NavBar.Brand>
-        </NavBar>
-        <CardModal show={show} onHide={handleClose} />
-        <CreateCategory click={handleShow} />
-        <CreateCategory click={handleShow} />
-      </div>
-      {list}
-      listhi penis
-    </div>
-  );
+    return (
+        <div className="background">
+            <div>
+                <HeaderBar/>
+                <CardModal show={show} onHide={handleClose} />
+                <CreateCategory click={handleShow} />
+                <CreateCategory click={handleShow} />
+            </div>
+            {list}
+            listhi penis
+        </div>
+    );
 }
 
 function CardModal(props) {
-  return (
-    <Modal {...props}>
-      <Modal.Header closeButton>cock</Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group controlId="testformcolor">
-            <Col>
-              <Row>
-                <Form.Label>Color</Form.Label>
-              </Row>
-              <Row>
-                <Button className="mb-2">Color</Button>
-              </Row>
-            </Col>
-          </Form.Group>
-          <Form.Group controlId="testform">
-            <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows="5" />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    return (
+        <Modal {...props}>
+            <Modal.Header closeButton>cock</Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group controlId="testformcolor">
+                        <Col>
+                            <Row>
+                                <Form.Label>Color</Form.Label>
+                            </Row>
+                            <Row>
+                                <Button className="mb-2">Color</Button>
+                            </Row>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="testform">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows="5" />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={props.onHide}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
 function CreateCategory(props) {
-  let buttons = [
-    { title: "cock", color: "red" },
-    { title: "and ball", color: "orange" },
-    { title: "torture", color: "green" },
-  ];
-  let list = [];
-  for (let i = 0; i < buttons.length; i++) {
-    let cName = "task " + buttons[i].color;
-    list.push(
-      <Row className="rowmargin">
-        <Button className={cName} onClick={props.click}>
-          {buttons[i].title}
-        </Button>
-      </Row>
-    );
-  }
+    let buttons = [
+        { title: "cock", color: "red" },
+        { title: "and ball", color: "orange" },
+        { title: "torture", color: "green" },
+    ];
+    let list = [];
+    for (let i = 0; i < buttons.length; i++) {
+        let cName = "task " + buttons[i].color;
+        list.push(
+            <Row className="rowmargin">
+                <Button className={cName} onClick={props.click}>
+                    {buttons[i].title}
+                </Button>
+            </Row>
+        );
+    }
 
-  return (
-    <Card className="container">
-      <Card.Header>
-        Cock
-        <Button variant="secondary" className="addcard float-right">
-          +
-        </Button>
-      </Card.Header>
-      <Card.Body className="text-center">
-        <Container fluid>{list}</Container>
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card className="container">
+            <Card.Header>
+                Cock
+                <Button variant="secondary" className="addcard float-right">
+                    +
+                </Button>
+            </Card.Header>
+            <Card.Body className="text-center">
+                <Container fluid>{list}</Container>
+            </Card.Body>
+        </Card>
+    );
 }
 
-export default App;
+function HeaderBar() {
+    return (
+        <NavBar variant="light" className="navbar">
+            <NavBar.Brand href="#">
+                <img
+                    src="jelli.png"
+                    width="30"
+                    height="26"
+                    className="d-inline-block align-top"
+                    alt="Jelli logo"
+                />
+                {"  "}Jelli
+            </NavBar.Brand>
+        </NavBar>
+    );
+}
+
+export {App,HeaderBar};
